2550b8d0d8770e49842ed2c9c199b8f5283b88ba
robbiekenny <robert_kenny@outlook.com>
2016-03-23 18:09:57 +0000

    play back of video functionality added

---
+++ HomeSecurityVerificationApp/Controllers/VideoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.MediaServices.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeSecurityVerificationApp.Controllers
+{
+    [RoutePrefix("Video")]
+    public class VideoController : Controller
+    {
+        [HttpGet]
+        [Route("PlayVideo/{video}")]
+        public ActionResult PlayVideo(string video)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(video);
+            string vid =  System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+
+            ViewBag.video = vid;
+            return View();
+        }
+    }
+}
